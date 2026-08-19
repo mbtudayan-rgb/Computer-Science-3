bb537c995f8cbf0bf3f4925d445aff50523c706e
Matt Jhardy <mbtudayan@carc.pshs.edu.ph>
2026-08-19 19:14:09 +0800

    Activity 1, ILA4

---
+++ main.py
@@ -0,0 +1,17 @@
+# math_utils.py
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def greet_user(name):
+    return f"Hello, {name}! Welcome to VS Code."
+
+def main():
+    print(add_numbers(20, 5))
+    print(multiply_numbers(4, 5))
+    print(greet_user("Matt Jhardy tudayan"))
+
+if __name__ == "__main__":
+    main()
